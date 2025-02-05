449068e02d8bb710f94f69ecf60536ab7f436682
Lauritz987 <lauritzritter4@gmail.com>
2025-02-05 22:37:06 +0100

    Updated server.js to serve PDF files

--- server.js
+++ server.js
@@ -1,30 +1,32 @@
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-
+const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
-// Set up static folder for serving uploaded files
-app.use(express.static(path.join(__dirname, 'uploads')));
+// Middleware to serve static files (PDFs) from the "uploads" folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// Route to get list of uploaded files
-app.get("/files", (req, res) => {
-    const uploadPath = path.join(__dirname, 'uploads');
-    fs.readdir(uploadPath, (err, files) => {
-        if (err) {
-            return res.status(500).send('Error reading the files');
-        }
-        res.json(files);
-    });
+// Serve the index.html file when accessing the root URL
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Serve the index.html file
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+// Endpoint to list all PDF files in the "uploads" folder
+app.get('/files', (req, res) => {
+    const fs = require('fs');
+    const uploadsDir = path.join(__dirname, 'uploads');
+    fs.readdir(uploadsDir, (err, files) => {
+        if (err) {
+            console.error('Error reading uploads directory:', err);
+            res.status(500).send('Error reading files');
+        } else {
+            const pdfFiles = files.filter(file => file.endsWith('.pdf'));
+            res.json(pdfFiles); // Send the list of PDF files
+        }
+    });
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
